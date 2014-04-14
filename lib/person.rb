34d9d2acfac200c078b9b22d738609b0e5ce7107
Nico Saueressig <nico@nicosaueressig.de>
2014-04-14 18:17:14 +0100

    4 tests pass, rent bike from station, crash

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,17 @@
 class Person
-
+	def initialize
+		@bike
+	end
+	
 	def has_bike?
+		!@bike.nil?
+	end
+
+	def rent_from(station)
+		@bike = station.release_bike
+	end
+
+	def crash!(bike)
+	@bike = bike.break!
 	end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -8,4 +8,24 @@ describe Person do
 		expect(person).not_to have_bike
 	end
 
+	it 'can rent a bike from a station' do
+		station = double :station
+		expect(station).to receive(:release_bike)
+		person.rent_from(station)
+	end
+
+	it 'receives bike when renting from station' do
+		station = double :station, release_bike: :bike
+		person.rent_from(station)
+		expect(person).to have_bike
+	end
+
+	it 'crashes' do
+		bike = double :bike
+		expect(bike).to receive(:break!)
+
+		person = Person.new
+		person.crash!(bike)
+	end
+
 end
